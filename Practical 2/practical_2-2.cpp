c71e3077a7eaf8bbebd6279d31fdf39ce04a6e9e
Shivanand Mitkar <73774388+shivanand0@users.noreply.github.com>
2022-07-02 10:57:28 +0530

    update

--- Practical 2/practical_2-2.cpp
+++ Practical 2/practical_2-2.cpp
@@ -46,3 +46,103 @@ int main() {
     }
     return 0;
 }
+
+/*
+JAVA 
+
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class SmithNumber {
+    static int getAddition(int num) {
+        int digit, sum = 0;
+        while (num > 0) {
+            // last digit of num
+            digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+        // System.out.println("num sum: "+sum);
+        return sum;
+    }
+
+    // Following are the steps to find all prime factors.
+    // 1) While n is divisible by 2, print 2 and divide n by 2.
+    // 2) After step 1, n must be odd. Now start a loop from i = 3 to the square
+    // root of n. While i divides n, print i, and divide n by i. After i fails to
+    // divide n, increment i by 2 and continue.
+    // 3) If n is a prime number and is greater than 2, then n will not become 1 by
+    // the above two steps. So print n if it is greater than 2.
+
+    //function to find prime factores of a number
+    static int primeFactors(int num)
+    {
+        ArrayList<Integer> factorsArrList = new ArrayList<Integer>();
+
+        int sum = 0;
+
+        while(num%2 == 0)
+        {
+            // System.out.println(2+ " *1 ");
+            factorsArrList.add(2);
+            num /= 2;
+        }
+
+        for (int i=3; i <= Math.sqrt(num); i+=2)
+        {
+            while(num%i == 0)
+            {
+                // System.out.println(i+ " *2 ");
+                factorsArrList.add(i);
+                num /= i;
+            }
+        }
+
+        if(num > 2)
+            factorsArrList.add(num);
+
+        for (int i = 0; i < factorsArrList.size(); i++) {
+            // System.out.println(factorsArrList.get(i));
+            sum = getAddition(factorsArrList.get(i)) + sum;
+        }
+        
+        // System.out.println("factors sum: "+sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        int num = 4937770;
+        boolean check=true;
+
+        while(check)
+        {
+            System.out.println(num);
+            int addition = getAddition(num);
+            int addition2 = primeFactors(num);
+
+            if(addition == addition2)
+            {
+                System.out.println(num+" is a Smith Number");
+                check = false;
+                break;
+            }
+            else
+            {
+                ++num;
+            }
+
+        }
+        // int addition = getAddition(num);
+        // // System.out.println(addition);
+        // int addition2 = primeFactors(num);
+
+        // if(addition == addition2)
+        //     System.out.println(num+" is a Smith Number");
+        // else
+        // {
+        //     int newNum = num++;
+        // }
+    }
+}
+
+
+*/
